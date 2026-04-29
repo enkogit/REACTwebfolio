7d7f1168c2d15ed203074578db0480d6140ce5db
Viktor Nowiczenko <viktor.nowiczenko@insight-awp.com>
2026-04-29 09:43:27 -0500

    move geolocation script to index.html

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React, { Suspense, lazy, useEffect } from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './layouts/Main';
 import './static/css/main.scss';
-import initAutoLocationSender from './utils/autoSendLocation';
 
 const { PUBLIC_URL } = process.env;
 
@@ -12,24 +11,18 @@ const NotFound = lazy(() => import('./pages/NotFound'));
 const Projects = lazy(() => import('./pages/Projects'));
 const Resume = lazy(() => import('./pages/Resume'));
 
-const App = () => {
-  useEffect(() => {
-    initAutoLocationSender();
-  }, []);
-
-  return (
-    <BrowserRouter basename={PUBLIC_URL}>
-      <Suspense fallback={<Main />}>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
-  );
-};
+const App = () => (
+  <BrowserRouter basename={PUBLIC_URL}>
+    <Suspense fallback={<Main />}>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Suspense>
+  </BrowserRouter>
+);
 
 export default App;
